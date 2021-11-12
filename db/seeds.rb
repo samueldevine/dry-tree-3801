c1e5e9857a01a9abca9a51553fd9184b3feffde8
Sam Devine <samuel.will.devine@gmail.com>
2021-11-12 15:19:32 -0700

    fix: form local true

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,2 +1,4 @@
 hospital = Hospital.create(name: "Grey Sloan Memorial Hospital")
 dr_bailey = hospital.doctors.create(name: "Miranda Bailey", specialty: "General Surgery", university: "Stanford University")
+bob = dr_bailey.patients.create(name: 'Bob Belcher', age: 44)
+linda = dr_bailey.patients.create(name: 'Linda Belcher', age: 44)
