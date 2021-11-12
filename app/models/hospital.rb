b507a2bf94467b7dfa6d616d02ae795e9af45ebe
Sam Devine <samuel.will.devine@gmail.com>
2021-11-12 14:20:45 -0700

    feat: hospital show page

--- app/models/hospital.rb
+++ app/models/hospital.rb
@@ -1,3 +1,15 @@
 class Hospital < ApplicationRecord
   has_many :doctors
+
+  def doctors_count
+    doctors.count
+  end
+
+  def doctor_universities
+    Hospital.joins(:doctors)
+      .where(id: id)
+      .select('doctors.university AS name')
+      .order('name')
+      .distinct
+  end
 end
