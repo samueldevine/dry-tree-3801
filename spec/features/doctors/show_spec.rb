1c42abf71515a65be5615fa34171c221d9784094
Sam Devine <samuel.will.devine@gmail.com>
2021-11-12 13:29:51 -0700

    test: doctor show page displays hospital and patients

--- spec/features/doctors/show_spec.rb
+++ spec/features/doctors/show_spec.rb
@@ -3,8 +3,8 @@ require 'rails_helper'
 RSpec.describe 'The Doctor Show Page' do
   describe 'displays' do
     before :each do
-      hospital = Hospital.create(name: "Grey Sloan Memorial Hospital")
-      @doctor = hospital.doctors.create(
+      @hospital = Hospital.create(name: "Grey Sloan Memorial Hospital")
+      @doctor = @hospital.doctors.create(
         name: "Miranda Bailey",
         specialty: "General Surgery",
         university: "Stanford University"
@@ -12,16 +12,32 @@ RSpec.describe 'The Doctor Show Page' do
       visit doctor_path(@doctor)
     end
 
-    it "a doctor's name" do
+    it "the doctor's name" do
       expect(page).to have_content @doctor.name
     end
 
-    it "a doctor's specialty" do
+    it "the doctor's specialty" do
       expect(page).to have_content @doctor.specialty
     end
 
-    it "a doctor's university" do
+    it "the doctor's university" do
       expect(page).to have_content @doctor.university
+    end
+
+    it "the doctor's hospital" do
+      expect(page).to have_content @hospital.name
+    end
+
+    it "the doctor's patients" do
+      @doctor.patients.create!(name: 'Bob Belcher', age: 44)
+      @doctor.patients.create!(name: 'Linda Belcher', age: 44)
+
+      visit doctor_path(@doctor)
+
+      within '#patients' do
+        expect(page).to have_content 'Bob Belcher'
+        expect(page).to have_content 'Linda Belcher'
+      end
     end
   end
 end
